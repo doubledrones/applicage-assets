5516866a0de99fddd472aec8b6d68d3014bedf1a
Marcin Nowicki <pr0d1r2@gmail.com>
2011-03-26 19:31:20 +0100

    Add base rails tree.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicageAssets::Application.config.secret_token = '2c77f71c4fb0914c00bd5df1539275c7683bcab5f5aa9d32c29dba32cae92b876883408f3003b870392f51bbe34e36f53a0d74c26b0f8b84d3cebc720f767e1c'
